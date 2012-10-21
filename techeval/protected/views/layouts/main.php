6ca96986a3b715ca6d2d131d95ce2a86631f2b97
Sherif Nosseir <sherifnosseir@gmail.com>
2012-10-21 15:27:51 -0700

    Tech eval update

---
+++ techeval/protected/views/layouts/main.php
@@ -0,0 +1,56 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+	<!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<div class="nav-collapse">
+					<ul class="nav">
+						<li class=""><a href="?r=site"><i class="icon-home"></i> Home</a></li>
+						<li class=""><a href="#"><i class="icon-signal"></i> Stats</a></li>
+					</ul>
+					
+					<ul class="nav pull-right">  
+						<li class="">
+							<?php $this->widget('zii.widgets.CMenu',array(
+								'items'=>array(
+									array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+									array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+								),
+							)); ?>
+						</li>
+					</ul>  
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
+<br>
+<div id="all-container">
+	<div class="container showgrid">
+		<div class="content">
+			<?php echo $content; ?>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
---
+++ techeval/protected/views/site/index.php
@@ -0,0 +1,17 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<div class="row well">
+	<div class="span4">
+		<li><a href="?r=main">Home Page</a></li>
+		<li><a href="?r=stats">Statistics Page</a></li>
+	</div>
+	
+	<div class="span6">
+		<h2 class="helpblock">Group 16 - Stars</h2>
+		<p>Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.</p>
+	</div>
+</div>
