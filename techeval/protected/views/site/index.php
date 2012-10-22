9938db30c124b43a5cd43bf295411d9e34c805d4
Sherif Nosseir <sherifnosseir@gmail.com>
2012-10-22 14:54:21 -0700

    Changed home page from normal container to fluid container

--- techeval/protected/views/site/index.php
+++ techeval/protected/views/site/index.php
@@ -4,14 +4,19 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<div class="row well">
-	<div class="span4">
-		<li><a href="?r=statistics/getAllMentions">Mentions Page</a></li>
-		<li><a href="?r=statistics">Statistics Page</a></li>
+<div class="container-fluid">
+<div class="row-fluid">
+	<div class="span3 well">
+		<ul class="nav nav-list">
+		  <li class="nav-header">Navigation</li>
+		  <li><a href="?r=statistics/getAllMentions">Mentions Page</a></li>
+		  <li><a href="?r=statistics">Statistics Page</a></li>
+		</ul>
 	</div>
 	
-	<div class="span6">
+	<div class="span9 well">
 		<h2 class="helpblock">Group 16 - Stars</h2>
 		<p>Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.</p>
 	</div>
+</div>
 </div>
